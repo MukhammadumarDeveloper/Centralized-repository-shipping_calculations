afdba4c58fabde43af6641a69646c5616e065ead
MukhammadumarDeveloper <Muhammadumar_Mahmudov@student.itpu.uz>
2025-08-06 06:53:10 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,5 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by Muhammadumar Mahmudov
+
+ # Here is another update by Muhammadumar Mahmudov
